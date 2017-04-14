ecaa865a651bc99534f8ea7d54332184227e8481
tairoroberto <tairoroberto@hotmail.com>
2017-04-14 18:32:25 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int showSumOfTwoVariables(){
+    int num, num2;
+    num = 10;
+    num2 = 500;
+
+    cout << "The sum of: " << num << " + " << num2 << " = " << num + num2;
+    return 0;
+}
+
+int main() {
+
+    /** Variables */
+
+    showSumOfTwoVariables();
+    return 0;
+}
